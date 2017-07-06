ca540827c5cfbc0578442bb7bc0ada38fcd41599
Surya Akhmad <surya.code111@gmail.com>
2017-07-06 18:47:31 +0000

    fulfillmentDeliverLex.js place order to zendesk

--- fulfillmentDeliverLex.js
+++ fulfillmentDeliverLex.js
@@ -1,7 +1,10 @@
 const AWS = require('aws-sdk');
 const https = require('https');
 const querystring = require('querystring');
-const s3 = new AWS.S3();
+const encrypted = process.env.zendesk;
+const zendeskHost = 'logibot.zendesk.com';
+let zendeskToken;
+
 let job;
 let tokenAccess;
 
@@ -11,7 +14,13 @@ function attachmentsHelper(){
             job.end_point[0]+','+job.end_point[1];
     imageStaticMap="https://maps.googleapis.com/maps/api/staticmap?"+params;
     return JSON.stringify([{ "color": "#2D9EB2", "attachment_type": "default",
-        "fields": [{"title":"Shipping Cost","value":"$"+job.cost,"short":true}],
+        "fields": [{"title":"Shipping Cost","value":"$"+job.cost,"short":true},
+        {"title":"Distance","value":job.distance+" km","short":true},
+        {"title":"Carried by","value":job.slots.vehicleType.toUpperCase(),"short":true},
+        {"title":"Payment Type","value":job.slots.paymentType,"short":true},
+        {"title":"Pickup Address","value":job.start_address},
+        {"title":"Delivery Address","value":job.end_address},
+        ],
         "image_url":imageStaticMap
     }]);
 }
@@ -56,24 +65,84 @@ function connectSlackApi(params, callback) {
     postReq.end();
 }
 
-function sendDetailOrder(){
-    
-    paramsToSlack={token:tokenAccess,channel:job.channel,text:'Thank you, <@'+job.userId+'>! \nYour order has been placed successfully',
+function connectZendesk(data, callback) {
+    const options={};
+    options.method="POST";
+    options.hostname=zendeskHost;
+    options.path='/api/v2/tickets.json';
+    options.headers= {
+        "Content-Type": "application/json",
+        "Authorization":"Basic "+zendeskToken
+    };
+    const postReq = https.request(options, (res) => {
+       const chunks = [];
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => chunks.push(chunk));
+        res.on('end', () => {
+            if (callback) {
+                callback({
+                    body: chunks.join(''),
+                    statusCode: res.statusCode,
+                    statusMessage: res.statusMessage,
+                });
+            }
+        });
+        return res;
+    });
+    postReq.write(data);
+    postReq.end();
+}
+
+function sendDetailOrder(ticketId){
+    paramsToSlack={token:tokenAccess,channel:job.channel,text:'Thank you, <@'+job.userId+'>! The order has been placed successfully\nYour tracking number is `'+ticketId+'`',
         attachments:attachmentsHelper()};
     connectSlackApi(paramsToSlack,(response)=>{
         res=JSON.parse(response.body);
     });
 }
 
-exports.handler = (event, context, callback) => {
-    console.log(event);
+function processEvent(event,context,callback){
     readDDB("Jobs","id = :id",{":id":event.sessionAttributes.JobsId},(err,data)=>{
         if(err) console.log("errJobsRead",JSON.stringify(err, null, 2));
         job=data.Items[0];
         readDDB("Team","team_id = :teamId",{":teamId":job.teamId},(errTeam,dataTeam)=>{
             if(errTeam) console.log("errTeam",JSON.stringify(errTeam, null, 2));
             tokenAccess=dataTeam.Items[0].bot.bot_access_token;
-            sendDetailOrder();
+            ticket={
+                ticket:{
+                    subject:"Placed Order from Slack",
+                    description:"Pickup Address: "+job.start_address+"\nDelivery Address: "+job.end_address,
+                    type:"task",
+                    tags:["sales"],
+                    custom_fields:[{id:114095637393,value:job.name},{id:114095591334,value:job.cost},{id:114095643114,value:job.distance},
+                        {id:114095651314,value:job.slots.paymentType},{id:114095637553,value:job.slots.vehicleType},
+                        {id:114095644734,value:job.start_point},{id:114095638933,value:job.end_point}
+                    ]
+                }
+            }
+            connectZendesk(JSON.stringify(ticket),res=>{
+                res=JSON.parse(res.body);
+                sendDetailOrder(res.ticket.id);
+            });
         });
     });
+}
+
+exports.handler = (event, context, callback) => {
+    console.log(event);
+    if (zendeskToken) {
+        processEvent(event, context, callback);
+    } else {
+        // Decrypt code should run once and variables stored outside of the function
+        // handler so that these are decrypted once per container
+        const kms = new AWS.KMS();
+        kms.decrypt({ CiphertextBlob: new Buffer(encrypted, 'base64') }, (err, data) => {
+            if (err) {
+                console.log('Decrypt error:', err);
+                return callback(err);
+            }
+            zendeskToken = data.Plaintext.toString('ascii');
+            processEvent(event, context, callback);
+        });
+    }
 };
